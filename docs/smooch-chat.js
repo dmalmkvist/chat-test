7f37dcf2b694d6cd6f73349fe5ad7823feba1ca1
Daniel Malmkvist <daniel@gladly.com>
2018-11-06 12:05:41 -0800

    hooked up authentication and sending an initial message

--- docs/smooch-chat.js
+++ docs/smooch-chat.js
@@ -1,4 +1,5 @@
 
+const SMOOCH_APP_ID = '5be0a05d88e4b900229d85f9';
 
 var authenticate = function(cb) {
     $.ajax({
@@ -6,13 +7,14 @@ var authenticate = function(cb) {
         type: 'POST',
         contentType: 'application/json;  charset=UTF-8',
         dataType: 'json',
-        data: {
+        data: JSON.stringify({
             email: 'test@example.com',
             name: "Carl Sagan",
-            loginDomain: "example.org"
-        },
+            loginDomain: "example.org",
+            appId: SMOOCH_APP_ID
+        }),
         success: function(response) {
-            console.log(response);
+            cb(response)
         },
         error: function() {
             console.log('auth failure');
@@ -22,8 +24,10 @@ var authenticate = function(cb) {
 
 var initSmooch = function(cb) {
     // To have our own UI
-    // Smooch.init({ appId: '5be0a05d88e4b900229d85f9', embedded: true }).then(cb);
-    Smooch.init({ appId: '5be0a05d88e4b900229d85f9' }).then(cb);
+    Smooch.init({ appId: SMOOCH_APP_ID, embedded: true }).then(cb);
+
+    // To use Smooch UI
+    //Smooch.init({ appId: SMOOCH_APP_ID }).then(cb);
 }
 
 var loginToSmooch = function(userId, jwt, onSuccess, onError) {
@@ -31,16 +35,45 @@ var loginToSmooch = function(userId, jwt, onSuccess, onError) {
     .then(onSuccess, onError);
 }
 
+var attachGladlyCustomerId = function(gladlyCustomerId) {
+    Smooch.setDelegate({
+        beforeSend: (message) => {
+            message.metadata = {
+                gladlyCustomerId: gladlyCustomerId
+            };
+            return message;
+        }
+    });
+}
+
+var sendMessage = function(gladlyCustomerId, message) {
+    console.log('Sending message to: ' + gladlyCustomerId + ', msg: ' + message);
+    Smooch.sendMessage({
+        type: 'text',
+        text: message
+    })
+}
+
 startChat = function() {
     console.log("Starting chat");
     initSmooch(function() {
-        authenticate(function(userId, jwt) {
+        console.log('Smooch initiated!')
+        authenticate(function(authResponse) {
+            console.log('authenticated with', authResponse);
+            let userId = authResponse.userId;
+            let gladlyCustomerId = authResponse.gladlyCustomerId;
+            let jwt = authResponse.jwt;
             loginToSmooch(userId, jwt, function() {
-                console.log("SUCCESS")
+                attachGladlyCustomerId(gladlyCustomerId);
+                sendMessage(gladlyCustomerId, 'This is working great!');
+                console.log("SUCCESS");
             }, function() {
-                console.log("FAILURE :(")
+                console.log("FAILURE :(");
             });
         })
     });
+
+    // If this is not called then the Smooch.init() callback will not be called
+    Smooch.render(document.getElementById('chat-container'));
 
 }
